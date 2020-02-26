35140790b776217d24563db866a84e8d4170f7f5
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-26 10:23:47 +0100

    wip

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Formik, Form, Field } from 'formik'
+import { useHistory } from 'react-router-dom'
 import axios from 'axios'
 
 const loginURL = 'http://localhost:3333/login'
 
 export default function Login(props) {
+  const history = useHistory()
   const onLogin = ({ username, password }) => {
     axios.post(
       loginURL,
@@ -13,6 +15,7 @@ export default function Login(props) {
       .then(res => {
         // successful login is not redirecting! :(
         localStorage.setItem('token', res.data.token)
+        history.push("/")
       })
       .catch(error => {
         debugger
